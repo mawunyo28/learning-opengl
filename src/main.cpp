34437afd5e5e28f9d42db3c1f426e355861634af
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2025-09-19 14:19:59 +0000

    Camera movements

--- src/main.cpp
+++ src/main.cpp
@@ -17,54 +17,25 @@ glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
-void frame_buffer_size_callback(GLFWwindow *window, int width, int height) {
-  glViewport(0, 0, width, height);
-}
+float yaw = 0.0f;
+float pitch = 0.0f;
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+float lastX = 400, lastY = 300;
+bool firstMouse = true;
+float Zoom = 0.0f;
+float fov = 45.0f;
 
-void changeOpacity(GLFWwindow *window, Shader shader) {
-  shader.use();
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 
-  float offset = 0.01f;
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 
-  if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-    float value;
-    value = shader.getFloat("opacity");
+void frame_buffer_size_callback(GLFWwindow *window, int width, int height);
 
-    if (value >= 0.0f && value <= 1.0f)
-      shader.setFloat("opacity", value + offset);
-  }
-  if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-    float value;
-    value = shader.getFloat("opacity");
+void changeOpacity(GLFWwindow *window, Shader shader);
 
-    if (value >= 0.1f)
-      shader.setFloat("opacity", value - offset);
-  }
-}
+void processInput(GLFWwindow *window, Shader *shader);
 
-void processInput(GLFWwindow *window, Shader *shader) {
-  const float cameraSpeed = 0.05f;
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, true);
-  }
-
-  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-    cameraPos += cameraSpeed * cameraFront;
-  }
-
-  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-    cameraPos -= cameraSpeed * cameraFront;
-  }
-  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-    cameraPos -=
-        cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
-  }
-  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-    cameraPos +=
-        cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
-  }
-  // changeOpacity(window, *shader);
-}
 int main() {
 
   glfwInit(); // Do this first always
@@ -89,6 +60,9 @@ int main() {
   }
   glfwSetFramebufferSizeCallback(
       window, (GLFWframebuffersizefun)frame_buffer_size_callback);
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwSetCursorPosCallback(window, mouse_callback);
+  glfwSetScrollCallback(window, scroll_callback);
 
   glViewport(0, 0, WIN_WIDTH, WIN_HEIGHT);
 
@@ -217,6 +191,9 @@ int main() {
   while (!glfwWindowShouldClose(window)) {
     processInput(window, &shader);
 
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
     // rendering
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -235,7 +212,7 @@ int main() {
 
     glm::mat4 projection;
     projection =
-        glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
 
     shader.setMat4("view", view);
     shader.setMat4("projection", projection);
@@ -257,4 +234,94 @@ int main() {
   }
 
   glfwTerminate();
+}
+
+void frame_buffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void changeOpacity(GLFWwindow *window, Shader shader) {
+
+  shader.use();
+
+  float offset = 0.01f;
+
+  if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+    float value;
+    value = shader.getFloat("opacity");
+
+    if (value >= 0.0f && value <= 1.0f)
+      shader.setFloat("opacity", value + offset);
+  }
+  if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+    float value;
+    value = shader.getFloat("opacity");
+
+    if (value >= 0.1f)
+      shader.setFloat("opacity", value - offset);
+  }
+}
+
+void processInput(GLFWwindow *window, Shader *shader) {
+  const float cameraSpeed = 2.50f * deltaTime;
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+    cameraPos += cameraSpeed * cameraFront;
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+    cameraPos -= cameraSpeed * cameraFront;
+  }
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+    cameraPos -=
+        cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+  }
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+    cameraPos +=
+        cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+  }
+  // changeOpacity(window, *shader);
+}
+
+void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
+  if (firstMouse) {
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+
+    lastX = width / 2;
+    lastY = height / 2;
+    firstMouse = false;
+  }
+  float xoffset = xpos - lastX;
+  float yoffset = lastY - ypos;
+  lastX = xpos;
+  lastY = ypos;
+  float sensitivity = 0.1f;
+  xoffset *= sensitivity;
+  yoffset *= sensitivity;
+  yaw += xoffset;
+  pitch += yoffset;
+  if (pitch > 89.0f)
+    pitch = 89.0f;
+  if (pitch < -89.0f)
+    pitch = -89.0f;
+  glm::vec3 direction;
+  direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+  direction.y = sin(glm::radians(pitch));
+  direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+  cameraFront = glm::normalize(direction);
+}
+
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
+  Zoom -= (float)yoffset;
+
+  if (Zoom < 1.0f)
+    Zoom = 1.0f;
+  if (Zoom > 45.0f)
+    Zoom = 45.0f;
+
+  fov = Zoom;
 }
